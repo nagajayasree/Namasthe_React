2b49fa5dbe27f73ec9613bbc93e8eae71fc346f4
JayaSree <js@eventdesk.co>
2023-12-17 19:49:29 -0500

    react elements and scripts

--- App.js
+++ App.js
@@ -1,20 +1,13 @@
-// const heading = React.createElement(
-//   "h1",
-//   { id: "heading" },
-//   "Hello from React!"
-// );
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
 const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement(
-    "div",
-    { id: "child" },
-    [
-      React.createElement("h1", {}, "I'm am a h1 tag"),
-      React.createElement("h2", {}, "I'm am a h2 tag"),
-    ]
-  )
+  'div',
+  { id: 'parent' },
+  React.createElement('div', { id: 'child' }, [
+    React.createElement('h1', {}, "I'm am a h1 tag"),
+    React.createElement('h2', {}, "I'm am a h2 tag"),
+  ])
 );
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(parent);
